19e8e7aaeec33d651613d9cb419d411ffdf8720d
Hyunjae <jhj940311@naver.com>
2022-08-26 17:43:38 +0900

    20220826 수업내용
    map, IO, network

--- 10_collection/src/edu/kh/collection/run/Run.java
+++ 10_collection/src/edu/kh/collection/run/Run.java
@@ -1,6 +1,7 @@
 package edu.kh.collection.run;
 
 import edu.kh.collection.model.service.ListService;
+import edu.kh.collection.model.service.MapService;
 import edu.kh.collection.model.service.SetService;
 import edu.kh.collection.view.StudentView;
 
@@ -14,9 +15,14 @@ public class Run {
 		
 //		new StudentView().displayMenu(); // 객체 1회성 사용
 		
-		SetService service2 = new SetService();
+//		SetService service2 = new SetService();
 //		service2.ex1();
 //		service2.ex2();
-		service2.ex3();
+//		service2.ex3();
+		
+		MapService service3 = new MapService();
+//		service3.ex1();
+//		service3.ex2();
+		service3.ex3();
 	}
 }
